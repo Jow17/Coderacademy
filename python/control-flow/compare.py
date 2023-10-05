41bb69ce62e4513961f3fb984511e30d4ce55473
Xphalagas <Jonotheowl@Outlook.com.au>
2023-10-05 16:40:54 +1100

    Initial commit

---
+++ python/control-flow/compare.py
@@ -0,0 +1,33 @@
+# x = int(input('What is X? '))
+# y = int(input('What is Y? '))
+
+# if x < y:
+#     print('X is less than Y')
+# elif x > y: #else if
+#     print('X is greater than Y')
+# else:
+#     print('X is equal to Y')
+
+score = int(input('Score: '))
+
+if score >= 90:
+  print('HD')
+elif score >= 80:
+  print('D')
+elif score >=70:
+  print('C')
+elif score >=50:
+  print('P')
+else:
+  print('F')
+
+
+# name = input('What is ur name? ')
+
+# match name: 
+#   case 'Harry' | 'Ron' | 'Hermoine':
+#    print('Gryffindor')
+#   case 'Draco':
+#    print('Slytherin')
+#   case _:
+#    print('Mudblood!')
