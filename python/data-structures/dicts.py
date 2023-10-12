a4b24d365708c018f9ae670efc33884819f31aae
Xphalagas <Jonotheowl@Outlook.com.au>
2023-10-12 11:46:43 +1100

    Initial Commit

---
+++ python/data-structures/dicts.py
@@ -0,0 +1,11 @@
+my_dog = { 'name': 'Ted', 'age' : 15, 'breed': 'Border Collie' }
+
+my_dog['age'] = 16 
+
+print (my_dog)
+
+dogs = [
+  {'name': 'Ted', 'age': 16, 'breed': 'Border Collie'},
+  ['name': 'Loki', 'age' ]
+   
+]
