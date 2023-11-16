d7d95822702df2c57eb582af54cbaacce7790a3f
Xphalagas <Jonotheowl@Outlook.com.au>
2023-11-16 13:44:59 +1100

    ORM part 2 finished

--- flask-lessons/trello-clone/app.py
+++ flask-lessons/trello-clone/app.py
@@ -1,19 +1,27 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from datetime import date
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://trello_dev:spameggs123@127.0.0.1:5432/trello'
 
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
 class Card(db.Model):
     __tablename__ = 'cards'
+
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     description = db.Column(db.Text())
+    status = db.Column(db.String(100))
     date_created = db.Column(db.Date())
+
+class CardSchema(ma.Schema):
+    class Meta: 
+        fields = ('id', 'title', 'description', 'status', 'date_created') 
 
 @app.cli.command('db_create')
 def db_create():
@@ -23,16 +31,39 @@ def db_create():
 
 @app.cli.command('db_seed')
 def db_seed():
-    card = Card(
+    cards = [
+    Card(
         title = 'Start the project',
         description = 'Stage 1 - Create ERD',
+        status = 'Done',
         date_created = date.today()
-    )
+    ),
+    Card(
+        title = 'ORM Queries',
+        description = 'Stage 2 - Implement CRUD queries',
+        status = 'In Progress',
+        date_created = date.today()
+    ),
+    Card(
+        title = 'Marshmellow',
+        description = 'Stage 3 - Implement JSONify of models',
+        status = 'In Progress',
+        date_created = date.today()
+    ),
+    ]
 
-    db.session.add(card)
+    db.session.add_all(cards)
     db.session.commit()
 
-print('Database seeded')
+    print('Database seeded')
+
+@app.route('/cards')
+def all_cards():
+    # select * from cards;
+    stmt = db.select(Card).where(db.or_(Card.status != 'Done', Card.id > 2)).order_by(Card.title.desc())
+    cards = db.session.scalars(stmt).all()
+    return CardSchema(many=True).dump(cards)
+
 @app.route('/')
 def index():
     return 'Hello, world!'
