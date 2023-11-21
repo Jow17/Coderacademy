5df3374d9f7ab954255484df54d4f748c4acefef
Xphalagas <Jonotheowl@Outlook.com.au>
2023-11-21 14:17:31 +1100

    Added verification

--- flask-lessons/trello-clone/app.py
+++ flask-lessons/trello-clone/app.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import date
 from flask_marshmallow import Marshmallow
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 
@@ -9,6 +10,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://trello_dev:spameg
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+bcrypt = Bcrypt(app)
 
 class Card(db.Model):
     __tablename__ = 'cards'
@@ -16,12 +18,25 @@ class Card(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     description = db.Column(db.Text())
-    status = db.Column(db.String(100))
+    status = db.Column(db.String(30))
     date_created = db.Column(db.Date())
 
 class CardSchema(ma.Schema):
     class Meta: 
         fields = ('id', 'title', 'description', 'status', 'date_created') 
+
+class User(db.Model):
+        __tablename__ = 'users'
+
+        id = db.Column(db.Integer, primary_key=True) 
+        name = db.Column(db.String) 
+        email = db.Column(db.String, nullable=False, unique=True) 
+        password = db.Column(db.String, nullable=False)
+        is_admin = db.Column(db.Boolean, default=False)
+
+class UserSchema(ma.Schema):
+     class Meta:
+          fields = ('id', 'name', 'email', 'password', 'is_admin')
 
 @app.cli.command('db_create')
 def db_create():
@@ -31,6 +46,18 @@ def db_create():
 
 @app.cli.command('db_seed')
 def db_seed():
+    users = [
+        User(
+            email='admin@spam.com',
+            password= bcrypt.generate_password_hash('spinynorman').decode('utf8'),
+            is_admin=True   
+        ),
+        User(
+            name = 'John Cleese',
+            email = 'cleese@spam.com',
+            password= bcrypt.generate_password_hash('tisbutascratch').decode('utf8'),
+        )
+    ]
     cards = [
     Card(
         title = 'Start the project',
@@ -52,10 +79,32 @@ def db_seed():
     ),
     ]
 
+    db.session.add_all(users)
     db.session.add_all(cards)
     db.session.commit()
 
     print('Database seeded')
+
+@app.route('/users/register', methods=['POST'])
+def register():
+     #Parse incoming POST body through the schema
+     user_info = UserSchema().load(request.json)
+     # Create a new user with the parsed data
+     user = User(
+          email = user_info['email'],
+          password = bcrypt.generate_password_hash(user_info['password']).decode('utf8'),
+          name = user_info.get('name', '')
+     )
+
+    # Add and commit the new user to the database
+     db.session.add(user)
+     db.session.commit()
+
+    # Return the new user
+     return UserSchema(exclude=['password']).dump(user), 201
+
+
+
 
 @app.route('/cards')
 def all_cards():
